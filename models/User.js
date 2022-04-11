58c9e896c39be619a48754f2686233db21fd39c9
Hannah Bush <heb.rutgers@gmail.com>
2022-04-11 17:51:46 -0400

    user routes configured, server.js set up

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const {Model, Datatypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create user model
+class User extends Model {}
+
+//define table columns and configuration
+User.init(
+  {
+    //table column definitions
+    //define id column
+    id: {
+      type:Datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: Datatypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        //at least 4 chars long
+        len: [4]
+      }
+    }
+  },
+  {
+    //table configuration options here (https://sequelize.org/v5/manual/models-definition.html#configuration)
+
+    //pass in imported sequelize connection
+    sequelize,
+    //dont automatically create createAt/UpdateAt timestamp fields
+    timestamps: false,
+    //dont pluralize name of db table
+    freezeTableName: true,
+    //ue underscores instead of camel-casing
+    underscored: true,
+    //make our model name stays lowercase in db
+    modelName:'user'
+  }
+);
+
+module.exports = User;
