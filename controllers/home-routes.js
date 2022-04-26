e6b585a81bac86c1a0a19f1304fcc7c735ad2dca
Hannah Bush <heb.rutgers@gmail.com>
2022-04-25 22:35:54 -0400

    user can successfully post a comment

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -57,12 +57,13 @@ router.get('/post/:id', (req, res) => {
       'id',
       'post_url',
       'title',
+      'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id'],
         include: {
           model: User,
           attributes: ['username']
@@ -74,15 +75,15 @@ router.get('/post/:id', (req, res) => {
       }
     ]
   })
-  .then(dbPostData => {
-    if(!dbPostData) {
-      res.status(404).json({ message: 'No post found with this id' });
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
         return;
       }
 
       // serialize the data
       const post = dbPostData.get({ plain: true });
-
+      console.log(post);
       // pass data to template
       res.render('single-post', { post });
     })
