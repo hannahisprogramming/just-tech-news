03c6e0d857d1c97ef085efbbba9e22245ee0c2e2
Hannah Bush <heb.rutgers@gmail.com>
2022-04-27 11:28:50 -0400

    added to dash routes, but posts not loading on dash

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,44 @@ const sequelize = require('../config/connection');
 const {Post, User, Comment} = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('dashboard', {loggedIn: true});
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      console.log(dbPostData);
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      console.log(posts);
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
